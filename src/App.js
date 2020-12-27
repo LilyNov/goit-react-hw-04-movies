8ba2ef941a08b73596f8ca89ec8040890984d0cd
Liliia Novikova <kplily85@gmail.com>
2020-12-27 12:14:08 +0200

    add header markup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import { Switch, Route } from 'react-router-dom';
+import Container from './Container/Container';
+import AppBar from './AppBar/AppBar';
+// import HomePage from './HomePage/HomePage';
+// import MoviesPage from './MoviesPage/MoviesPage';
+
+export default function App() {
+  return (
+    <Container>
+      <AppBar />
+    </Container>
+  );
+}
